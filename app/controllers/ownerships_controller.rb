ea57c0a08a7ed1e0e5d772d94290d0347df14838
nagakuray <naruhodo.u1@icloud.com>
2017-01-04 20:11:23 +0900

    setting.ymlとsecret.ymlで設定できるように修正

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,26 +1,32 @@
 class OwnershipsController < ApplicationController
   require 'open-uri'
-  before_action :get_slide_list, only: [:new]
+  require 'openssl'
 
   def new
+    @slides = get_slide_list
   end
 
   private
     def get_slide_list
       if params[:q]
-        url ="https://www.slideshare.net/api/2/search_slideshows"
+        url = EasySettings.url_search_slideshows
         api_param = Hash.new
         api_param["q"] = params[:q]
-        api_param["api_key"] = 'DF37gXpB'
-        api_param["sharedsecret"] = 'MAcsjIcU'
+        api_param["api_key"] = Rails.application.secrets.api_key
+        api_param["sharedsecret"] = Rails.application.secrets.sharedsecret
         api_param["ts"] = Time.now.to_i.to_s
         api_param["hash"] = Digest::SHA1.hexdigest(api_param["sharedsecret"]+api_param["ts"])
         api_param["lang"] = "ja"
+        # api_param["page"] = 5.to_s
+        # api_param["items_per_page"] = 20.to_s
 
         uri_param = api_param.sort.map {|i| i.join('=')}.join('&')
         uri = url + "?" + URI.escape(uri_param)
         xml = Nokogiri::XML(open(uri))
-        @slides = xml.xpath("//Slideshow")
+        slides = xml.xpath("//Slideshow")
+
+        return slides
+
       end
     end
 
